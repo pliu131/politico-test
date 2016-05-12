bd3010285bce542c854d723cdc0e7e7b3d836886
Peter Liu <pliu7@binghamton.edu>
2016-05-12 02:06:28 -0400

    test pagespeed optimizations

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,76 +7,89 @@ module.exports = function(grunt) {
       options: {
         map: true,
         processors: [
-          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
-          ],
-          dist: {
-            src: 'css/*.css'
-          }
-        }
-      },
-      htmlmin: {
+        require('autoprefixer')({browsers: 'last 2 versions'}),
+        ],
         dist: {
-          options: {
-            removeComments: true,
-            collapseWhitespace: true,
-            minifyCSS: true,
-            minifyJS: true,
-            minifyURLs: true
-          },
-          files: {                           
-            'index.html': 'dev/index.html'
-          }
-        }
-      },
-      sass: {
-        dist: {
-          options: {
-            style: 'compressed',
-            require: 'susy'
-          },
-          files: {
-            'css/main.css': 'css/main.scss'
-          }
-        }
-      },
-      connect: {
-        server: {
-          options: {
-            livereload: true,
-            // to run this server using only the grunt connect command
-            keepalive: true 
-          }
-        }
-      },
-      watch: {
-        options: {
-          livereload: true
-        },
-        css: {
-          files: '**/*.scss',
-          tasks: ['sass'],
-          options: {
-            interrupt: true
-          }
-        },
-        reload: {
-          files: [
-          "*.html",
-          "js/*.js"
-          ]
-        },
-        post: {
-          files: 'css/*.css',
-          tasks: ['postcss']
+          src: 'css/*.css'
         }
       }
-    });
+    },
+    critical: {
+      dist: {
+        options: {
+          inline: true,
+          base: './',
+          css: ['css/main.css'],
+          width: 320,
+          height: 560
+        },
+        src: 'dev/index.html',
+        dest: 'dev/index-critical.html'
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+          minifyCSS: true,
+          minifyJS: true,
+          minifyURLs: true
+        },
+        files: {                           
+          'index.html': 'dev/index-critical.html'
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          require: 'susy'
+        },
+        files: {
+          'css/main.css': 'css/main.scss'
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          livereload: true,
+          keepalive: true 
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass'],
+        options: {
+          interrupt: true
+        }
+      },
+      reload: {
+        files: [
+        "*.html",
+        "js/*.js"
+        ]
+      },
+      post: {
+        files: 'css/*.css',
+        tasks: ['postcss']
+      }
+    }
+  });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-postcss');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-critical');
 
   grunt.registerTask('default', ['sass', 'connect', 'watch']);
 
