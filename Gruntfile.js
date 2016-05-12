120a996bd26cde672b699beffbb05bc7a3f2ed3f
Peter Liu <pliu7@binghamton.edu>
2016-05-11 23:15:46 -0400

    run grunt tasks to minify css and html

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,59 +8,73 @@ module.exports = function(grunt) {
         map: true,
         processors: [
           require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
-        ],
+          ],
+          dist: {
+            src: 'css/*.css'
+          }
+        }
+      },
+      htmlmin: {
         dist: {
-          src: 'css/*.css'
+          options: {
+            removeComments: true,
+            collapseWhitespace: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: true
+          },
+          files: {                           
+            'index.html': 'dev/index.html'
+          }
         }
-      }
-    },
-    sass: {
-      dist: {
-        options: {
-          style: 'expanded',
-          require: 'susy'
-        },
-        files: {
-          'css/main.css': 'css/main.scss'
+      },
+      sass: {
+        dist: {
+          options: {
+            style: 'compressed',
+            require: 'susy'
+          },
+          files: {
+            'css/main.css': 'css/main.scss'
+          }
         }
-      }
-    },
-    connect: {
-      server: {
+      },
+      connect: {
+        server: {
+          options: {
+            livereload: true
+          }
+        }
+      },
+      watch: {
         options: {
           livereload: true
-        }
-      }
-    },
-    watch: {
-      options: {
-        livereload: true
-      },
-      css: {
-        files: '**/*.scss',
-        tasks: ['sass'],
-        options: {
-          interrupt: true
-        }
-      },
-      reload: {
-        files: [
+        },
+        css: {
+          files: '**/*.scss',
+          tasks: ['sass'],
+          options: {
+            interrupt: true
+          }
+        },
+        reload: {
+          files: [
           "*.html",
           "js/*.js"
-        ]
-      },
-      post: {
-        files: 'css/*.css',
-        tasks: ['postcss']
+          ]
+        },
+        post: {
+          files: 'css/*.css',
+          tasks: ['postcss']
+        }
       }
-    }
-  });
+    });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-postcss');
-
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
   grunt.registerTask('default', ['sass', 'connect', 'watch']);
 
